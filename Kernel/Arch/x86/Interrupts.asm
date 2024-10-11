6d69689ff8c29a148c3617891b600bd169b12582
Magyar57 <alxbrp@gmail.com>
2024-10-11 02:53:08 +0200

    ISR: Renamed some Interrupts functions

--- Kernel/Arch/x86/Interrupts.asm
+++ Kernel/Arch/x86/Interrupts.asm
@@ -1,15 +1,15 @@
 bits 32
 
-; void x86_disable_interrupts();
-global x86_disable_interrupts
-x86_disable_interrupts:
+; void x86_DisableInterrupts();
+global x86_DisableInterrupts
+x86_DisableInterrupts:
 	cli
 	ret
-; END x86_disable_interrupts
+; END x86_DisableInterrupts
 
-; void x86_enable_interrupts();
-global x86_enable_interrupts
-x86_enable_interrupts:
+; void x86_EnableInterrupts();
+global x86_EnableInterrupts
+x86_EnableInterrupts:
 	sti
 	ret
-; END x86_enable_interrupts
+; END x86_EnableInterrupts
