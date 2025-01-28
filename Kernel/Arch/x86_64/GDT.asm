fccb5f3ec443907291847698497108905320fc45
Magyar57 <alxbrp@gmail.com>
2025-01-29 00:51:40 +0100

    GDT: fixed for long mode

--- Kernel/Arch/x86_64/GDT.asm
+++ Kernel/Arch/x86_64/GDT.asm
@@ -1,6 +1,6 @@
-bits 64
+section .text
 
-; void setGDT(GDT_LocationDescriptor_64* descriptor, uint16_t kcodeSegment, uint16_t kdataSegment);
+; void setGDT(GDTLocationDescriptor* descriptor, uint16_t kcodeSegment, uint16_t kdataSegment);
 ; in System V ABI,   i386 (32 bits): descriptor in ebp+8 ; kcodeSegment in ebp+12 ; kdataSegment in ebp+16
 ; in System V ABI, x86_64 (64 bits): descriptor in rdi   ; kcodeSegment in rsi    ; kdataSegment in rdx
 global setGDT
@@ -9,18 +9,16 @@ setGDT:
 	mov rbp, rsp
 
 	; load gdt ; address given by 'descriptor' arg
-	; mov eax, [ebp+8]
-	; lgdt [eax]
 	lgdt [rdi]
 
 	; setup segments registers
-	; mov rax, [rbp+12]	; ax = kcodeSegment
-	; push rax
 	push rsi
-	push .reload_cs	; cs cannot be mv-ed directly, we have to jump
-	retf				; would be equivalent to jmp eax : .reload_cs
+	lea rax, [rel .reload_cs]	; cs cannot be mv-ed directly, we have to jump
+	push rax
+	retfq						; would be equivalent to jmp eax : .reload_cs
 	.reload_cs:
-	mov rax, rdx	; ax = kdataSegment
+
+	mov rax, rdx	; rax = kdataSegment
 	mov ds, ax
 	mov es, ax
 	mov fs, ax
--- Kernel/Arch/x86_64/GDT.c
+++ Kernel/Arch/x86_64/GDT.c
@@ -6,19 +6,17 @@
 
 // =========== GDT Entries ===========
 
-// A GDT_Entry is a segment descriptor
+// A GDTEntry is a segment descriptor
 // Base  (32-bit value): linear address where the segment begins
 // Limit (20-bit value): tells maximum addressable unit (controlled by the granularity)
 typedef struct {
 	uint16_t limit_0to15;				// Limit (bit 0-15)
 	uint16_t base_0to15;				// Base (bit 0-15)
 	uint8_t base_16to23;				// Base (bit 16-23)
-	uint8_t accessByte;					// Access byte (see GDT_ACCESS)
+	uint8_t accessByte;					// Access byte (see GDTAccess)
 	uint8_t limit_16to19_and_flags;		// Limit (bit 16-19) followed by flags (on 4 bits)
 	uint8_t base_24to31;				// Base (bit 24-31)
-	uint32_t base;						// Base (bit 32-63) (long mode only)
-	uint32_t reserved;					// (long mode only)
-} __attribute__((packed)) GDT_Entry;
+} __attribute__((packed)) GDTEntry;
 
 // Access bytes, bits:
 // 7:   Present
@@ -28,7 +26,7 @@ typedef struct {
 // 2:   Direction/Conforming bit
 // 1:   Readable/writable bit
 // 0:   Accessed bit
-enum GDT_ACCESS {
+enum GDTAccess {
 	GDT_ACCESS_PRESENT =			0b10000000, // Present = 1: valid entry/segment (0=invalid entry)
 
 	GDT_ACCESS_RING0 =				0b00000000, // Ring 0 (kernel)
@@ -56,7 +54,7 @@ enum GDT_ACCESS {
 // 2: Size flag
 // 1: Long-mode code flag
 // 0: Reserved
-enum GDT_Flags {
+enum GDTFlags {
 	GDT_FLAG_GRANULARITY_1B =	0b0000, // Segment size granularity of 1 Byte
 	GDT_FLAG_GRANULARITY_4K =	0b1000, // Segment size granularity of 4 KiB
 
@@ -69,17 +67,11 @@ enum GDT_Flags {
 
 // =========== Descriptors ===========
 
-// GDT Descriptor (32 bits mode)
-typedef struct {
-	uint16_t size;		// GDT size -1
-	uint32_t offset;	// GDT address in memory
-} __attribute__((packed)) GDT_LocationDescriptor_32;
-
-// GDT Descriptor (64 bits mode)
+// GDT Descriptor (long mode)
 typedef struct {
 	uint16_t size;		// GDT size -1
 	uint64_t offset;	// GDT address in memory
-} __attribute__((packed)) GDT_LocationDescriptor_64;
+} __attribute__((packed)) GDTLocationDescriptor;
 
 // =========== GDT generation maccros ===========
 
@@ -89,32 +81,30 @@ typedef struct {
 #define GDT_get_limit_16to19_and_flags(limit, flags)	(uint8_t)  (((limit >> 16) & 0x0f) | (((flags)<<4) & 0xf0))
 #define GDT_get_base_24to31(base)						(uint8_t)  ((base >> 24) & 0xff)
 
-#define GDT_get_GDT_Entry(base, limit, access, flags) \
-	{ GDT_get_limit_0to15(limit), GDT_get_base_0to15(base), GDT_get_base_16to23(base), access, GDT_get_limit_16to19_and_flags(limit, flags), GDT_get_base_24to31(base), 0, 0 }
+#define GDT_get_GDTEntry(base, limit, access, flags) \
+	{ GDT_get_limit_0to15(limit), GDT_get_base_0to15(base), GDT_get_base_16to23(base), access, GDT_get_limit_16to19_and_flags(limit, flags), GDT_get_base_24to31(base) }
 
 // =========== Declare GDT and assign GDT entries ===========
 
 #define GDT_KERNEL_CODE_ACCESS GDT_ACCESS_PRESENT|GDT_ACCESS_RING0|GDT_ACCESS_TYPE_CODE_OR_DATA|GDT_ACCESS_EXEC_ALLOW|GDT_ACCESS_DC_UP|GDT_ACCESS_RW_ALLOW
 #define GDT_KERNEL_DATA_ACCESS GDT_ACCESS_PRESENT|GDT_ACCESS_RING0|GDT_ACCESS_TYPE_CODE_OR_DATA|GDT_ACCESS_EXEC_FORBID|GDT_ACCESS_DC_UP|GDT_ACCESS_RW_ALLOW
-#define GDT_KERNEL_CODE_FLAGS  GDT_FLAG_GRANULARITY_4K|GDT_FLAG_SIZE_32_PMODE|GDT_FLAG_LONG_MODE_OFF
+#define GDT_KERNEL_CODE_FLAGS  GDT_FLAG_GRANULARITY_4K|GDT_FLAG_LONG_MODE_ON
 #define GDT_KERNEL_DATA_FLAGS  GDT_FLAG_GRANULARITY_4K|GDT_FLAG_SIZE_32_PMODE|GDT_FLAG_LONG_MODE_OFF
 
 // Global GDT variable, in (kernel) memory
-GDT_Entry g_GDT[] = {
-	// Null descriptor
-	GDT_get_GDT_Entry(0x0, 0x0, 0, 0),
-	// Kernel 32-bit code segment
-	GDT_get_GDT_Entry(0x0, 0xfffff, GDT_KERNEL_CODE_ACCESS, 0xc),
-	// Kernel 32-bit data segment
-	GDT_get_GDT_Entry(0x0, 0xfffff, GDT_KERNEL_DATA_ACCESS, GDT_KERNEL_DATA_FLAGS)
+// TODO replace hardcoded values with the macros above
+GDTEntry g_GDT[] = {
+	GDT_get_GDTEntry(0x0, 0x0, 0, 0),				// Null descriptor
+	GDT_get_GDTEntry(0x0, 0xffffffff, 0x9a, 0xa),	// Kernel 32-bit code segment
+	GDT_get_GDTEntry(0x0, 0xffffffff, 0x92, 0xc)	// Kernel 32-bit data segment
 };
 
 // Global GDT location descriptor, in (kernel) memory
-GDT_LocationDescriptor_64 g_GDTLocationDescriptor = { sizeof(g_GDT)-1, (uint64_t) g_GDT };
+GDTLocationDescriptor g_GDTLocationDescriptor = { sizeof(g_GDT)-1, (uint64_t) g_GDT };
 
 // setGDT - Defined in GDT.asm
 // Sets the GDT located at 'descriptor' and loads the segments registers accordingly (kcodeSegment & kdataSegment)
-void __attribute__((cdecl)) setGDT(GDT_LocationDescriptor_64* descriptor, uint16_t kcodeSegment, uint16_t kdataSegment);
+void __attribute__((cdecl)) setGDT(GDTLocationDescriptor* descriptor, uint16_t kcodeSegment, uint16_t kdataSegment);
 
 void GDT_initialize(){
 	setGDT(&g_GDTLocationDescriptor, GDT_SEGMENT_KTEXT, GDT_SEGMENT_KDATA);
