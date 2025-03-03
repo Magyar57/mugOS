f2a0c33228353be3ef3f841579da04757ec76800
Magyar57 <alxbrp@gmail.com>
2025-03-03 23:38:37 +0100

    x86 IRQChip: added spurious IRQ handling & blinking timer notification

--- Kernel/Arch/x86_64/IRQChip.c
+++ Kernel/Arch/x86_64/IRQChip.c
@@ -18,13 +18,13 @@ IRQHandler g_IRQHandlers[16];
 static const char* const g_IRQTypes[] = {
     "Programmable Interrupt Timer (PIT)",
     "PS/2 Keyboard",
-    "Cascade",											// Used internally by the two PICs, never raised
-    "COM2",												// If enabled
-    "COM1",												// If enabled
-    "LPT2",												// If enabled
+    "Cascade",								// Used internally by the two PICs, never raised
+    "COM2",									// If enabled
+    "COM1",									// If enabled
+    "LPT2",									// If enabled
     "Floppy Disk",
-    "LPT1 / Unreliable 'spurious' interrupt",			// Usually (?)
-    "CMOS real-time clock",								// If enabled
+    "LPT1",
+    "CMOS real-time clock",					// If enabled
     "Free for peripherals/legacy SCSI/NIC",
     "Free for peripherals/legacy SCSI/NIC",
     "Free for peripherals/legacy SCSI/NIC",
@@ -47,25 +47,45 @@ void IRQChip_deregisterHandler(uint8_t irq){
 // ================ IRQ Handlers ================
 
 static void prehandler(struct ISR_Params* params){
-	// If we have a handler to call, we call it, and 'alles gut'
-
 	uint8_t irq = (uint8_t) params->vector;
 	irq = irq - IRQ_MASTER_PIC_REMAP_OFFSET; // clamp irq to 0-15
 
-	if (g_IRQHandlers[irq] != NULL){
-		g_IRQHandlers[irq](params);
-		i8259_sendEIO(irq);
-		return;
+	// IRQ 7 and 15 may be spurious, in which case we must NOT send an EOI
+	if (irq==7 || irq==15){
+		uint16_t isr = i8259_getCombinedISR();
+		// Spurious IRQ 7
+		if ((isr & (1<<7)) == 0){
+			log(WARNING, MODULE, "Got spurious IRQ %d, ignored (no EOI sent)", irq);
+			return;
+		}
+		// Spurious IRQ 15
+		// We don't still need to ACK the interrupt to the slave PIC,
+		//  but we do for the master PIC though
+		if ((isr & (1<<15)) == 0){
+			log(WARNING, MODULE, "Got spurious IRQ %d, ignored (no EOI sent)", irq);
+			i8259_sendEIO(7); // Master PIC ACK
+			return;
+		}
 	}
 
-	// Otherwise we PANIC !
-	const char* interrupt_type = g_IRQTypes[irq];
-	log(PANIC, MODULE, "Unhandled IRQ number %d - %s", irq, interrupt_type);
-	panic();
+	// If we have a handler to call, we call it, and 'alles gut'
+	if (g_IRQHandlers[irq] != NULL) g_IRQHandlers[irq](params);
+	// Otherwise, we send a warning
+	else log(WARNING, MODULE, "Unhandled IRQ number %d - %s", irq, g_IRQTypes[irq]);
+
+	// Finally, signal to the PIC that we handled the interrupt
+	i8259_sendEIO(irq);
 }
 
+// Simple blinking timer on the bottom right of the screen
+#include "Drivers/Graphics/Framebuffer.h"
 void timer(struct ISR_Params* params){
-	// Nothing to do yet
+	extern Framebuffer m_framebuffer;
+	static bool clock = false;
+	const int rect_size = 4;
+	color_t color = (clock) ? GREEN : LIGHT_GREY;
+	Framebuffer_drawRectangle(&m_framebuffer, m_framebuffer.width-rect_size-1, m_framebuffer.height-rect_size-1, rect_size, rect_size, color);
+	clock = !clock;
 }
 
 void IRQChip_initialize(){
