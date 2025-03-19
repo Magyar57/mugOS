688d3a4f78e153c6e0938a2a2046233d6fbf08d4
Magyar57 <alxbrp@gmail.com>
2025-03-20 00:25:30 +0100

    PS/2 Controller HAL: replaced maccros with always_inline functions

--- Kernel/Arch/x86_64/Include/HAL/PS2Controller.h
+++ Kernel/Arch/x86_64/Include/HAL/PS2Controller.h
@@ -1,30 +1,40 @@
 #ifndef __PS2_CONTROLLER_H__
 #define __PS2_CONTROLLER_H__
 
+#include "Preprocessor.h"
 #include "Arch/x86_64/Drivers/i8042.h"
 
-#define PS2Controller_initialize i8042_initialize
+always_inline void PS2Controller_initialize(){
+	i8042_initialize();
+}
 
 /// @returns Returns whether the controller/driver is initialized and enabled, and which ports are enabled
-#define PS2Controller_getStatus i8042_getStatus
+always_inline void PS2Controller_getStatus(bool *isEnabled_out, bool *port1Available_out, bool *port2Available_out, bool *translationOut){
+	i8042_getStatus(isEnabled_out, port1Available_out, port2Available_out, translationOut);
+}
 
-/// @brief Enable the interrupts sent from the devices
-#define PS2Controller_enableDevicesInterrupts i8042_enableDevicesInterrupts
-
-/// @brief Disable the interrupts sent from the devices
-#define PS2Controller_disableDevicesInterrupts i8042_disableDevicesInterrupts
+/// @brief Enable/disable the IRQs sent from the devices
+always_inline void PS2Controller_setDevicesIRQ(bool device1, bool device2){
+	i8042_setDevicesIRQ(device1, device2);
+}
 
 // Send a byte to the PS/2 device 1
 // Returns: true on success, false on failure
-#define PS2Controller_sendByteToDevice1 i8042_sendByteToDevice1
+always_inline bool PS2Controller_sendByteToDevice1(uint8_t byte){
+	return i8042_sendByteToDevice1(byte);
+}
 
 /// @brief Send a byte to the PS/2 device 2
 /// @returns true on success, false on failure
-#define PS2Controller_sendByteToDevice2 i8042_sendByteToDevice2
+always_inline bool PS2Controller_sendByteToDevice2(uint8_t byte){
+	return i8042_sendByteToDevice2(byte);
+}
 
 /// @brief Receive a byte from a previously sent command
 /// @returns true on success, false on failure (no readable byte in output buffer at the end of timeout).
 /// If success, write output to *byte_out
-#define PS2Controller_receiveByte i8042_receiveByte
+always_inline bool PS2Controller_receiveByte(uint8_t* byte_out){
+	return i8042_receiveByte(byte_out);
+}
 
 #endif
