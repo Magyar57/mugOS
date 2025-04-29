e78333a05bfd93f0dfb9199f2331d1934aef27cd
Magyar57 <alxbrp@gmail.com>
2025-04-29 02:18:27 +0200

    VMM: added VMM_initialize and VMM_getKernelMemoryOffset

--- Kernel/Memory/VMM.c
+++ Kernel/Memory/VMM.c
@@ -1,11 +1,19 @@
-// #include <stdint.h>
+#include "HAL/Paging.h"
 
 #include "Memory/VMM.h"
 
 uint64_t m_kernelMemoryOffset = 0xffffffffffffffff; // init with invalid offset
 
+void VMM_initialize(){
+	Paging_initialize();
+}
+
 void VMM_setKernelMemoryOffset(uint64_t offset){
 	m_kernelMemoryOffset = offset;
+}
+
+uint64_t VMM_getKernelMemoryOffset(){
+	return m_kernelMemoryOffset;
 }
 
 virtual_address_t VMM_physicalToVirtual(physical_address_t addr){
--- Kernel/Memory/VMM.h
+++ Kernel/Memory/VMM.h
@@ -5,7 +5,9 @@
 
 // VMM.h: Virtual Memory Manager
 
+void VMM_initialize();
 void VMM_setKernelMemoryOffset(uint64_t offset);
+uint64_t VMM_getKernelMemoryOffset();
 virtual_address_t VMM_physicalToVirtual(physical_address_t addr);
 physical_address_t VMM_virtualToPhysical(virtual_address_t addr);
 
