d64b2ff9e91d7b8fe497d8a3b1df2380417e9871
Magyar57 <alxbrp@gmail.com>
2025-07-05 13:16:56 +0200

    Framebuffer: now using MMIO

--- Kernel/Drivers/Graphics/Framebuffer.c
+++ Kernel/Drivers/Graphics/Framebuffer.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include "string.h"
 #include "assert.h"
+#include "IO.h"
 #include "IRQ.h"
 #include "Logging.h"
 #include "Drivers/Graphics/Font.h"
@@ -51,9 +52,10 @@ void Framebuffer_clearScreen(Framebuffer* this){
 	for(uint64_t j=0 ; j<this->height ; j++){
 		size_t lineOffset = j*one_line_offset;
 		for (uint64_t i=0 ; i<this->width ; i++){
-			this->address[lineOffset + i] = this->clearColor;
+			writeRelaxed32(this->address + lineOffset + i, this->clearColor);
 		}
 	}
+	writeMemoryBarrier();
 }
 
 void Framebuffer_drawLetter(Framebuffer* this, unsigned char letter, uint32_t fontColor, unsigned int offsetX, unsigned int offsetY){
@@ -64,6 +66,7 @@ void Framebuffer_drawLetter(Framebuffer* this, unsigned char letter, uint32_t fo
 	const uint8_t* bitmap = DEFAULT_FONT[letter];
 	int mask; // bit mask, will be shifted right every iteration
 	color_t colorToDraw;
+	uint32_t* pixel;
 
 	// Cache as many things as possible, since this is a critical section (performance-wise)
 	const uint64_t one_line_offet = getLineOffset();
@@ -86,13 +89,15 @@ void Framebuffer_drawLetter(Framebuffer* this, unsigned char letter, uint32_t fo
 			for (uint64_t jj=0 ; jj<zoom ; jj++){
 				uint64_t iterationLineOffset = baseLineOffset + jj*one_line_offet;
 				for (uint64_t ii=0 ; ii<zoom ; ii++){
-					this->address[iterationLineOffset + horizontalOffset+ii] = colorToDraw;
+					pixel = this->address + iterationLineOffset + horizontalOffset + ii;
+					writeRelaxed32(pixel, colorToDraw);
 				}
 			}
 
 			mask >>= 1; // next bitmap pixel
 		}
 	}
+	writeMemoryBarrier();
 }
 
 // (Re)draws the entire screen. IRQ UNSAFE !
@@ -224,7 +229,7 @@ void Framebuffer_putPixel(Framebuffer* this, unsigned int x, unsigned int y, col
 	assert(y < this->height);
 
 	const int offset = getLineOffset();
-	this->address[offset*y + x] = pixel;
+	write32(this->address + offset*y + x, pixel);
 }
 
 void Framebuffer_drawRectangle(Framebuffer* this, unsigned int x, unsigned int y, unsigned int width, unsigned int height, color_t c){
@@ -239,19 +244,25 @@ void Framebuffer_drawRectangle(Framebuffer* this, unsigned int x, unsigned int y
 
 	// Top line
 	line_cache = y*offset;
-	for (unsigned int i=line_cache+x ; i<line_cache+final_x ; i++) this->address[i] = c;
+	for (unsigned int i=line_cache+x ; i<line_cache+final_x ; i++)
+		write32(this->address + i, c);
 
 	// Left line
 	// line_cache = y*offset; // but line_cache is the same, no need to recompute it
-	for (unsigned int j=line_cache+x ; j<offset*final_y ; j+=offset) this->address[j] = c;
+	for (unsigned int j=line_cache+x ; j<offset*final_y ; j+=offset)
+		write32(this->address + j, c);
 
 	// Right line
 	// line_cache is still the same
-	for (unsigned int j=line_cache+final_x-1 ; j<offset*final_y ; j+=offset) this->address[j] = c;
+	for (unsigned int j=line_cache+final_x-1 ; j<offset*final_y ; j+=offset)
+		write32(this->address + j, c);
 
 	// Bottom line
 	line_cache = (final_y-1)*offset;
-	for (unsigned int i=line_cache+x ; i<line_cache+final_x ; i++) this->address[i] = c;
+	for (unsigned int i=line_cache+x ; i<line_cache+final_x ; i++)
+		write32(this->address + i, c);
+
+	writeMemoryBarrier();
 }
 
 void Framebuffer_fillRectangle(Framebuffer* this, unsigned int x, unsigned int y, unsigned int width, unsigned int height, color_t c){
@@ -267,7 +278,7 @@ void Framebuffer_fillRectangle(Framebuffer* this, unsigned int x, unsigned int y
 	for (unsigned int j=y ; j<final_y ; j++) {
 		line_cache = offset*j;
 		for (unsigned int i=x ; i<final_x ; i++) {
-			this->address[line_cache + i] = c;
+			write32(this->address + line_cache + i, c);
 		}
 	}
 }
