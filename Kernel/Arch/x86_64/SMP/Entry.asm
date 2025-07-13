d248d55c38a331e7475d7bdee625ab86a0dd75da
Magyar57 <alxbrp@gmail.com>
2025-07-13 02:30:16 +0200

    Added Auxiliary Processor (AP) entry point

---
+++ Kernel/Arch/x86_64/SMP/Entry.asm
@@ -0,0 +1,14 @@
+; Entry.asm: Entry point for the non-BSP processors
+
+; The CPUs start in 16 bits real mode
+bits 16
+
+; The entry point for APs gets its own section, as it needs special alignment
+; The 'exec' is a flag for the linker, to get proper debugging
+section .ap_entry exec
+
+global entryAP
+entryAP:
+	cli
+	hlt
+	jmp entryAP
