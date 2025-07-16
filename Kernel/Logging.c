bc338f3fcb3e9380a559ca62d2da634645014bdb
Magyar57 <alxbrp@gmail.com>
2025-07-17 00:06:37 +0200

    Logging: added hexdump & NULL handling

--- Kernel/Logging.c
+++ Kernel/Logging.c
@@ -36,6 +36,10 @@ static inline bool formatLogString(char* str, size_t n, int logLevel, const char
 
 void Logging_log(int logLevel, const char* moduleName, const char* logFmtStr, ...){
 	if (logLevel<0 || logLevel>PANIC) return;
+	if (logFmtStr == NULL){
+		Logging_log(logLevel, moduleName, "(null)");
+		return;
+	}
 
 	va_list args;
 	va_start(args, logFmtStr);
@@ -56,4 +60,68 @@ void Logging_log(int logLevel, const char* moduleName, const char* logFmtStr, ..
 	// sendStringDefault returns false if the Serial is uninitialized AND on error
 	// For simplicity, we ignore errors
 	Serial_sendStringDefault(buff);
+}
+
+void Logging_hexdump(int logLevel, const char* moduleName, void* addr , size_t n){
+	if (logLevel<0 || logLevel>PANIC)return;
+
+	if (addr == NULL){
+		Logging_log(logLevel, moduleName, "Hexdump: (null)");
+		return;
+	}
+
+	char buff[512];
+	uint8_t* data = addr;
+	size_t i;
+	int printed;
+
+	// Dump 16 bytes per line
+	for (i=0 ; i < n-15 ; i+=16){
+		printed = snprintf(buff, sizeof(buff),
+			"Hexdump: %#.8llx - "
+			"%.2hhx %.2hhx %.2hhx %.2hhx %.2hhx %.2hhx %.2hhx %.2hhx  "
+			"%.2hhx %.2hhx %.2hhx %.2hhx %.2hhx %.2hhx %.2hhx %.2hhx",
+			i,
+			data[i], data[i+1], data[i+2], data[i+3], data[i+4], data[i+5], data[i+6], data[i+7],
+			data[i+8], data[i+9], data[i+10], data[i+11], data[i+12], data[i+13], data[i+14], data[i+15]
+		);
+		if (printed < 0) {
+			Logging_log(ERROR, MODULE, "snprintf failure, couldn't dump memory");
+			return;
+		}
+		Logging_log(logLevel, moduleName, buff);
+	}
+
+	// Final, partial line
+
+	if (n % 16 == 0)
+		return;
+
+	printed = snprintf(buff, sizeof(buff), "Hexdump: %#.8llx - ", i);
+	if (printed < 0) {
+		Logging_log(ERROR, MODULE, "snprintf failure, couldn't dump memory");
+		return;
+	}
+
+	int offset = printed;
+
+	for ( ; i<n ; i++){
+		printed = snprintf(buff+offset, sizeof(buff)-i-offset, "%.2hhx ", data[i]);
+		if (printed < 0){
+			Logging_log(ERROR, MODULE, "snprintf failure, couldn't dump memory");
+			return;
+		}
+		offset += printed;
+
+		// Add space separator in the middle
+		if (i % 8 == 7){
+			printed = snprintf(buff+offset, sizeof(buff)-i-offset, " ");
+			if (printed < 0){
+				Logging_log(ERROR, MODULE, "snprintf failure, couldn't dump memory");
+				return;
+			}
+			offset += printed;
+		}
+	}
+	Logging_log(logLevel, moduleName, buff);
 }
--- Kernel/Logging.h
+++ Kernel/Logging.h
@@ -8,9 +8,19 @@
 #define ERROR 4
 #define PANIC 5
 
+void Logging_log(int logLevel, const char* moduleName, const char* logFmtStr, ...);
+
+/// @brief Dump the memory (in hexadecimal) in the console
+void Logging_hexdump(int logLevel, const char* moduleName, void* addr , size_t n);
+
+/// @brief Alias for Logging_log
 #define log Logging_log
+
+/// @brief Alias for `Logging_log`, with default arguments.
+/// Handy for writing quick logs while debugging
 #define debug(fmt, ...) Logging_log(DEBUG, __FILE__, fmt, ##__VA_ARGS__)
 
-void Logging_log(int logLevel, const char* moduleName, const char* logFmtStr, ...);
+/// @brief Debug alias for Logging_hexdump (similar to the `debug` alias)
+#define hexdump(addr, n) Logging_hexdump(DEBUG, __FILE__, addr, n)
 
 #endif
