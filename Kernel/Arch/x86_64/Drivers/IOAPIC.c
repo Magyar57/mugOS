690e7bcb702daf8fdafff31d835dab6aaa0abeb2
Magyar57 <alxbrp@gmail.com>
2025-07-30 23:35:49 +0200

    I/O APIC: added multiple chip support & adapted initialization

--- Kernel/Arch/x86_64/Drivers/IOAPIC.c
+++ Kernel/Arch/x86_64/Drivers/IOAPIC.c
@@ -1,9 +1,11 @@
 #include <stdint.h>
+#include "stdlib.h"
 #include "IO.h"
 #include "Panic.h"
 #include "Logging.h"
 #include "IRQ.h"
 #include "Memory/VMM.h"
+#include "SMP/SMP.h"
 #include "Drivers/ACPI/ACPI.h"
 
 #include "IOAPIC.h"
@@ -50,14 +52,16 @@ union RedirectionReg {
 };
 
 struct IOAPIC {
-	void* address;
+	void* address; // Memory-mapped I/O, usable (aka in virtual memory)
 	volatile uint32_t* selectReg;
 	volatile uint32_t* windowReg;
-	uint32_t id;
+	uint8_t id;
+	uint32_t GSIBase;
 	union VersionRegister version;
 };
 
-static struct IOAPIC m_IOAPIC; // only using one I/O APIC is supported
+static int m_nIOAPIC;
+static struct IOAPIC* m_IOAPICs;
 
 static inline uint32_t readRegister32(struct IOAPIC* ioapic, uint8_t reg){
 	write32(ioapic->selectReg, reg);
@@ -79,18 +83,17 @@ static inline void writeRegister64(struct IOAPIC* ioapic, uint8_t reg, uint64_t 
 }
 
 /// @brief Init a specific I/O APIC
-static void initIOAPIC(struct IOAPIC* ioapic, int n){
+static void initIOAPIC(struct IOAPIC* ioapic){
 	// Map the memory-mapped registers in the VMM
-	physical_address_t ioapic_phys = g_MADT.IOAPICs[n].address;
+	physical_address_t ioapic_phys = (physical_address_t) ioapic->address;
 	virtual_address_t ioapic_virt = ioapic_phys | VMM_KERNEL_MEMORY;
 	VMM_map(ioapic_phys, ioapic_virt, 1, PAGE_READ|PAGE_WRITE|PAGE_KERNEL|PAGE_CACHE_DISABLED);
 
-	m_IOAPIC.address = (void*) ioapic_virt;
-	m_IOAPIC.selectReg = m_IOAPIC.address + IOAPIC_REG_SELECT;
-	m_IOAPIC.windowReg = m_IOAPIC.address + IOAPIC_REG_WINDOW;
-
-	m_IOAPIC.id = g_MADT.IOAPICs[n].ID;
-	m_IOAPIC.version.value = readRegister32(ioapic, IOAPIC_REG_VERSION);
+	ioapic->address = (void*) ioapic_virt;
+	ioapic->selectReg = ioapic->address + IOAPIC_REG_SELECT;
+	ioapic->windowReg = ioapic->address + IOAPIC_REG_WINDOW;
+	ioapic->version.value = readRegister32(ioapic, IOAPIC_REG_VERSION);
+	int n_pins = ioapic->version.bits.maxRedirectionEntry + 1;
 
 	union RedirectionReg redirection;
 	redirection.value = 0;
@@ -99,42 +102,56 @@ static void initIOAPIC(struct IOAPIC* ioapic, int n){
 	for (int i=0 ; i<g_MADT.nIOAPIC_ISO ; i++){
 		struct MADTEntry_IOAPIC_ISO* override = g_MADT.IOAPIC_ISOs + i;
 
+		if (override->GSI < ioapic->GSIBase || override->GSI >= ioapic->GSIBase+n_pins)
+			continue;
+
 		if (override->busSource != 0){
 			log(WARNING, MODULE, "Ignored non-ISA IRQ override (IRQ=%d GSI=%d)",
 				override->IRQSource, override->GSI);
 			continue;
 		}
 
-		redirection.bits.vector = ISA_IRQ_OFFSET + override->IRQSource;
+		int pin = override->GSI - ioapic->GSIBase;
+		uint8_t vector = ISA_IRQ_OFFSET + override->IRQSource;
+
+		redirection.bits.vector = vector;
 		redirection.bits.deliveryMode = 0b000;
 		redirection.bits.destinationMode = 0; // physical
 		redirection.bits.pinPolarity = override->flags.bits.pinPolarity;
 		redirection.bits.triggerMode = override->flags.bits.triggerMode;
 		redirection.bits.masked = false;
-		redirection.bits.destination = 0; // LAPIC 0
-		writeRegister64(ioapic, IOAPIC_REG_REDIR_TABLE(override->GSI), redirection.value);
+		redirection.bits.destination = SMP_getCpuId(); // Send IRQs to boostrap CPU
+		writeRegister64(ioapic, IOAPIC_REG_REDIR_TABLE(pin), redirection.value);
+
+		// log(INFO, MODULE, "configured I/O APIC %d pin %2d to IRQ %2d (GSI=%2d)",
+		// 	ioapic->id, pin, vector, override->GSI);
 	}
 
-	// Then, initialize all un-mentionned ISA IRQ to default
-	int n_iso_apic = min(16, m_IOAPIC.version.bits.maxRedirectionEntry+1);
-	for (int i=0 ; i<n_iso_apic ; i++){
+	// Then, identity-map all un-mentionned IRQs
+	for (int i=0 ; i<n_pins ; i++){
 		redirection.value = readRegister64(ioapic, IOAPIC_REG_REDIR_TABLE(i));
 		// Only setup the interrupts that were not in the overrides
 		if (!redirection.bits.masked)
 			continue;
 
-		redirection.bits.vector = ISA_IRQ_OFFSET + i;
+		int GSI = ioapic->GSIBase + i;
+		uint8_t vector = ISA_IRQ_OFFSET + GSI;
+
+		redirection.bits.vector = vector;
 		redirection.bits.deliveryMode = 0b000;
 		redirection.bits.destinationMode = 0; // physical
 		redirection.bits.pinPolarity = 0;
 		redirection.bits.triggerMode = 0;
 		redirection.bits.masked = false;
-		redirection.bits.destination = 0; // LAPIC 0
+		redirection.bits.destination = SMP_getCpuId();
 		writeRegister64(ioapic, IOAPIC_REG_REDIR_TABLE(i), redirection.value);
+
+		// log(INFO, MODULE, "configured I/O APIC %d pin %2d to IRQ %2d (GSI=%2d)",
+		// 	ioapic->id, i, vector, GSI);
 	}
 
-	log(INFO, MODULE, "Initialized I/O APIC (ID=%d, version %d, #IRQ=%d)",
-		m_IOAPIC.id, m_IOAPIC.version.bits.version, m_IOAPIC.version.bits.maxRedirectionEntry+1);
+	log(SUCCESS, MODULE, "Initialized I/O APIC (ID=%d, version %d, #IRQ=%d, GSIBase=%d)",
+		ioapic->id, ioapic->version.bits.version, n_pins, ioapic->GSIBase);
 }
 
 void IOAPIC_init(){
@@ -148,8 +165,14 @@ void IOAPIC_init(){
 		panic();
 	}
 
-	if (g_MADT.nIOAPIC > 1)
-		log(INFO, MODULE, "System has several I/O APICs, only one will be used");
+	m_nIOAPIC = g_MADT.nIOAPIC;
+	m_IOAPICs = (struct IOAPIC*) kmalloc(m_nIOAPIC * sizeof(struct IOAPIC));
 
-	initIOAPIC(&m_IOAPIC, 0);
+	for (int i=0 ; i<m_nIOAPIC ; i++){
+		m_IOAPICs[i].address = (void*) (uintptr_t) (g_MADT.IOAPICs[i].address);
+		m_IOAPICs[i].id = g_MADT.IOAPICs[i].ID;
+		m_IOAPICs[i].GSIBase = g_MADT.IOAPICs[i].GSIBase;
+
+		initIOAPIC(m_IOAPICs + i);
+	}
 }
