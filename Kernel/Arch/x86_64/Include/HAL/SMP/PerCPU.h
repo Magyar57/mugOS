8dc06d32305d4847eea8441028a1e5ced22d8347
Magyar57 <alxbrp@gmail.com>
2025-08-25 00:13:26 +0200

    PerCPU: added PerCPU_setCPUInfoMember

--- Kernel/Arch/x86_64/Include/HAL/SMP/PerCPU.h
+++ Kernel/Arch/x86_64/Include/HAL/SMP/PerCPU.h
@@ -3,7 +3,6 @@
 
 #include <stdint.h>
 
-// SMP-related, architecture-agnostic CPU informations
 struct CPUInfo {
 	uint32_t ID; // the actual ID we use, from 0 to #CPUS-1
 	uint32_t apicID;
@@ -17,7 +16,7 @@ extern struct CPUInfo* g_CPUInfos;
 
 /// @brief Get the value of the `member` (of type `type`) from the per-CPU struct CPUInfo instance
 #define PerCPU_getCPUInfoMember(member) ({ \
-	__typeof__(((struct CPUInfo *)0)->member) __val; \
+	__typeof__(((struct CPUInfo*) 0)->member) __val; \
 	/* '%c1' ensures the offset is treated as a compile-time constant */ \
 	__asm__ volatile ("mov %%gs:%c1, %0" \
 		: "=r" (__val) \
@@ -25,6 +24,16 @@ extern struct CPUInfo* g_CPUInfos;
 		: "memory"); /* Clobber memory to prevent reordering */ \
 	__val; \
 })
+
+/// @brief Set the value of the `member` in the per-CPU struct CPUInfo instance
+#define PerCPU_setCPUInfoMember(member, value) \
+do { \
+	__typeof__(((struct CPUInfo*) 0)->member) __val = (value); \
+	__asm__ volatile ("mov %0, %%gs:%c1" \
+		: /* No outputs */ \
+		: "r" (__val), "i" (offsetof(struct CPUInfo, member)) \
+		: "memory"); \
+} while (0)
 
 #define PerCPU_getCpuId() PerCPU_getCPUInfoMember(ID)
 
