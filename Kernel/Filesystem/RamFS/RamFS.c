7d3e36a963aa48e8db4609340fdc5e52ea42f66b
Magyar57 <alxbrp@gmail.com>
2025-12-24 13:06:02 +0100

    RamFS: added functionnal interfaces

--- Kernel/Filesystem/RamFS/RamFS.c
+++ Kernel/Filesystem/RamFS/RamFS.c
@@ -4,9 +4,32 @@
 #include "RamFS.h"
 #define MODULE "RamFS"
 
+static struct FilesystemFunctions m_fsFuncs = {
+	.allocNode = NULL,
+	.freeNode = NULL
+};
+
+struct NodeFunctions m_nodeFuncs = {
+	.create = NULL,
+	.remove = NULL,
+	.rename = NULL,
+	.mkdir = NULL,
+	.rmdir = NULL
+};
+
+struct FileFunctions m_FileFuncs = {
+	.open = NULL,
+	.read = NULL,
+	.write = NULL,
+	.flush = NULL,
+	.close = NULL
+};
+
 struct Filesystem m_ramfs = {
 	.name = "ramfs",
-	.private = NULL
+	.private = NULL,
+
+	.funcs = &m_fsFuncs
 };
 
 void RamFS_init(){
